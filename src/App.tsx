d03503c28b10b9b3a2478961a25f59e25374f67f
bharat948 <bharatpatidar.002@gmail.com>
2026-06-20 11:59:07 +0530

    Remove /play page and routing completely

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,10 @@ import About from './sections/About';
 import Projects from './sections/Projects';
 import Skills from './sections/Skills';
 import Contact from './sections/Contact';
-import Play from './pages/Play';
-import { useRoute } from './lib/router';
 import { useDarkMode } from './hooks/useDarkMode';
 
 function App() {
   const { darkMode, toggleTheme } = useDarkMode();
-  const route = useRoute();
-
-  if (route === '/play') {
-    return <Play darkMode={darkMode} toggleTheme={toggleTheme} />;
-  }
 
   return (
     <Layout darkMode={darkMode} toggleTheme={toggleTheme}>
